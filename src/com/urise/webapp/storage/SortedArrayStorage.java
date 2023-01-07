0b8f2f9d16966cb24ff7b516ef91c2d47c49258a
Egor <egor19942012@gmail.com>
2023-01-07 16:48:09 +0700

    Refactor SortedArrayStorage
    Replace shifting elements via iteration with shifting via System.arraycopy()

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -8,18 +8,16 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void insertResume(Resume resume, int index) {
         int insertIndex = -index - 1;
-        for (int i = size; i > insertIndex; i--) {
-            storage[i] = storage[i - 1];
-        }
+        System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
         storage[insertIndex] = resume;
     }
 
     @Override
     public void deleteResume(int index) {
-        for (int i = index; i < size - 1; i++) {
-            storage[i] = storage[i + 1];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
         }
-        storage[size - 1] = null;
     }
 
     @Override
