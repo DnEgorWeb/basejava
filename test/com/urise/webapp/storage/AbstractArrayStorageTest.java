3d1a6597ddb7f2ac257f3ed651e60c120fc86c31
Egor <egor19942012@gmail.com>
2023-01-09 20:16:20 +0700

    Generate AbstractArrayStorageTest

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,35 @@
+package com.urise.webapp.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractArrayStorageTest {
+    @Test
+    void get() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
