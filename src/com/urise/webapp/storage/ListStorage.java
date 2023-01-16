b00ea917afc321d119171b9974b2baf4d1acf641
Egor <egor19942012@gmail.com>
2023-01-16 20:40:45 +0700

    Add ListStorage and ListStorageTest classes

---
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -0,0 +1,66 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.Resume;
+
+import java.util.ArrayList;
+
+public class ListStorage implements Storage {
+    protected final ArrayList<Resume> storage = new ArrayList<>();
+    @Override
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        return storage.get(index);
+    }
+
+    @Override
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index > -1) {
+            throw new ExistStorageException(r.getUuid());
+        } else {
+            storage.add(r);
+        }
+    }
+
+    @Override
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(r.getUuid());
+        }
+        storage.set(index, r);
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        storage.remove(index);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.toArray(new Resume[0]);
+    }
+
+    private int getIndex(String uuid) {
+        return storage.indexOf(new Resume(uuid));
+    }
+}
