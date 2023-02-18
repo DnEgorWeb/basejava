927cab8320e6bc1f469cdb7457cc05dc42994438
Egor <egor19942012@gmail.com>
2023-02-18 14:27:34 +0700

    Refactor doWrite() of DataStreamSerializer

--- src/com/urise/webapp/storage/serialization/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serialization/DataStreamSerializer.java
@@ -25,36 +25,11 @@ public class DataStreamSerializer implements StreamSerializer {
                 String className = dis.readUTF();
                 AbstractSection section = null;
                 if (className.equals(ListSection.class.getName())) {
-                    int listSize = dis.readInt();
-                    List<String> list = new ArrayList<>();
-                    for (int j = 0; j < listSize; j++) {
-                        String val = dis.readUTF();
-                        list.add(val);
-                    }
-                    section = new ListSection(list);
+                    section = new ListSection(readList(dis));
                 } else if (className.equals(TextSection.class.getName())) {
                     section = new TextSection(dis.readUTF());
                 } else if (className.equals(CompanySection.class.getName())) {
-                    int companiesSize = dis.readInt();
-                    List<Company> companies = new ArrayList<>();
-                    for (int k = 0; k < companiesSize; k++) {
-                        String name = dis.readUTF();
-                        String websiteNullMarker = dis.readUTF();
-                        String website = websiteNullMarker.equals("null") ? null : dis.readUTF();
-                        int periodsSize = dis.readInt();
-                        List<Company.Period> periods = new ArrayList<>();
-                        for (int l = 0; l < periodsSize; l++) {
-                            LocalDate startDate = LocalDate.parse(dis.readUTF());
-                            String endDateNullMarker = dis.readUTF();
-                            LocalDate endDate = endDateNullMarker.equals("null") ? null : LocalDate.parse(dis.readUTF());
-                            String title = dis.readUTF();
-                            String descriptionNullMarker = dis.readUTF();
-                            String description = descriptionNullMarker.equals("null") ? null : dis.readUTF();
-                            periods.add(new Company.Period(startDate, endDate, title, description));
-                        }
-                        companies.add(new Company(name, website, periods));
-                    }
-                    section = new CompanySection(companies);
+                    section = new CompanySection(readCompanies(dis));
                 }
                 resume.addSection(sectionType, section);
             }
@@ -67,61 +42,121 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
-            Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
+            writeContacts(dos, r.getContacts());
+            writeSections(dos, r.getSections());
+        }
+    }
+
+    private List<String> readList(DataInputStream dis) throws IOException {
+        int listSize = dis.readInt();
+        List<String> list = new ArrayList<>();
+        for (int j = 0; j < listSize; j++) {
+            String val = dis.readUTF();
+            list.add(val);
+        }
+        return list;
+    }
+
+    private List<Company> readCompanies(DataInputStream dis) throws IOException {
+        int companiesSize = dis.readInt();
+        List<Company> companies = new ArrayList<>();
+        for (int k = 0; k < companiesSize; k++) {
+            String name = dis.readUTF();
+            String websiteNullMarker = dis.readUTF();
+            String website = websiteNullMarker.equals("null") ? null : dis.readUTF();
+            List<Company.Period> periods = readPeriods(dis);
+            companies.add(new Company(name, website, periods));
+        }
+        return companies;
+    }
+
+    private List<Company.Period> readPeriods(DataInputStream dis) throws IOException {
+        int periodsSize = dis.readInt();
+        List<Company.Period> periods = new ArrayList<>();
+        for (int l = 0; l < periodsSize; l++) {
+            LocalDate startDate = LocalDate.parse(dis.readUTF());
+            String endDateNullMarker = dis.readUTF();
+            LocalDate endDate = endDateNullMarker.equals("null") ? null : LocalDate.parse(dis.readUTF());
+            String title = dis.readUTF();
+            String descriptionNullMarker = dis.readUTF();
+            String description = descriptionNullMarker.equals("null") ? null : dis.readUTF();
+            periods.add(new Company.Period(startDate, endDate, title, description));
+        }
+        return periods;
+    }
+
+    private void writeContacts(DataOutputStream dos, Map<ContactType, String> contacts) throws IOException {
+        dos.writeInt(contacts.size());
+        for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            dos.writeUTF(entry.getKey().name());
+            dos.writeUTF(entry.getValue());
+        }
+    }
+
+    private void writeSections(DataOutputStream dos, Map<SectionType, AbstractSection> sections) throws IOException {
+        dos.writeInt(sections.size());
+        for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
+            dos.writeUTF(entry.getKey().name());
+            AbstractSection section = entry.getValue();
+            switch (section.getClass().getSimpleName()) {
+                case "ListSection":
+                    writeListSection(dos, (ListSection) section);
+                    break;
+                case "TextSection":
+                    writeTextSection(dos, (TextSection) section);
+                    break;
+                case "CompanySection":
+                    writeCompanySection(dos, (CompanySection) section);
+                    break;
+                default:
+                    throw new IllegalStateException("Unknown section type");
             }
-            Map<SectionType, AbstractSection> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                AbstractSection section = entry.getValue();
-                if (section instanceof ListSection) {
-                    ListSection ls = (ListSection) section;
-                    dos.writeUTF(ListSection.class.getName());
-                    List<String> list = ls.getList();
-                    int listSize = list.size();
-                    dos.writeInt(listSize);
-                    for (String s : list) {
-                        dos.writeUTF(s);
-                    }
-                } else if (section instanceof TextSection) {
-                    TextSection ts = (TextSection) section;
-                    dos.writeUTF(TextSection.class.getName());
-                    dos.writeUTF(ts.getText());
-                } else if (section instanceof CompanySection) {
-                    CompanySection cs = (CompanySection) section;
-                    dos.writeUTF(CompanySection.class.getName());
-                    List<Company> companies = cs.getCompanies();
-                    dos.writeInt(companies.size());
-                    for (Company company : companies) {
-                        dos.writeUTF(company.getName());
-                        boolean isWebsiteNull = company.getWebsite() == null;
-                        dos.writeUTF(isWebsiteNull ? "null" : "not-null");
-                        if (!isWebsiteNull) {
-                            dos.writeUTF(company.getWebsite());
-                        }
-                        List<Company.Period> periods = company.getPeriods();
-                        dos.writeInt(periods.size());
-                        for (Company.Period period : periods) {
-                            dos.writeUTF(period.getStartDate().toString());
-                            boolean isEndDateNull = period.getEndDate() == null;
-                            dos.writeUTF(isEndDateNull ? "null" : "not-null");
-                            if (!isEndDateNull) {
-                                dos.writeUTF(period.getEndDate().toString());
-                            }
-                            dos.writeUTF(period.getTitle());
-                            boolean isDescriptionNull = period.getDescription() == null;
-                            dos.writeUTF(isDescriptionNull ? "null" : "not-null");
-                            if (!isDescriptionNull) {
-                                dos.writeUTF(period.getDescription());
-                            }
-                        }
-                    }
-                }
-            }
+        }
+    }
+
+    private void writeListSection(DataOutputStream dos, ListSection section) throws IOException {
+        dos.writeUTF(ListSection.class.getName());
+        List<String> list = section.getList();
+        int listSize = list.size();
+        dos.writeInt(listSize);
+        for (String s : list) {
+            dos.writeUTF(s);
+        }
+    }
+
+    private void writeTextSection(DataOutputStream dos, TextSection section) throws IOException {
+        dos.writeUTF(TextSection.class.getName());
+        dos.writeUTF(section.getText());
+    }
+
+    private void writeCompanySection(DataOutputStream dos, CompanySection section) throws IOException {
+        dos.writeUTF(CompanySection.class.getName());
+        writeCompanies(dos, section.getCompanies());
+    }
+
+    private void writeCompanies(DataOutputStream dos, List<Company> companies) throws IOException {
+        dos.writeInt(companies.size());
+        for (Company company : companies) {
+            dos.writeUTF(company.getName());
+            writeNullableUTF(dos, company.getWebsite());
+            writePeriods(dos, company.getPeriods());
+        }
+    }
+
+    private void writePeriods(DataOutputStream dos, List<Company.Period> periods) throws IOException {
+        dos.writeInt(periods.size());
+        for (Company.Period period : periods) {
+            dos.writeUTF(period.getStartDate().toString());
+            writeNullableUTF(dos, period.getEndDate());
+            dos.writeUTF(period.getTitle());
+            writeNullableUTF(dos, period.getDescription());
+        }
+    }
+
+    private void writeNullableUTF(DataOutputStream dos, Object o) throws IOException {
+        dos.writeUTF(o == null ? "null" : "");
+        if (o != null) {
+            dos.writeUTF(o.toString());
         }
     }
 }
